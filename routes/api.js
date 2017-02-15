8047765b02a12e2f34fe07d6dbec719215f43b35
AntoineLacoste <antoine.lacoste@ynov.com>
2017-02-15 10:55:48 +0100

    initial commit

---
+++ routes/api.js
@@ -0,0 +1,57 @@
+var express  = require('express');
+var router   = express.Router();
+var User     = require('../model/user');
+var passport = require('passport');
+var verify   = require('../verify');
+
+router.all('/*', verify.verifyUser, function (req, res, next) {
+    next();
+});
+
+router.get('/', function(req, res, next) {
+    res.json({
+       'status': 200,
+        'message': 'Welcome !'
+    });
+});
+router.get('/all', function(req, res, next) {
+    User.find({}).then(function (user) {
+        res.json({users: user});
+    }, function (err) {
+        console.log(err);
+    });
+});
+
+router.post('/login', function (req, res, next) {
+    console.log("toto");
+    passport.authenticate('local', function (err, user, info) {
+        console.log(err);
+        req.logIn(user, function (err) {
+            console.log(err);
+            var token = verify.getToken(user);
+            res.json(token);
+        })
+    })(req, res, next);
+});
+
+// router.post('/', function(req, res, next) {
+//     var user = new User({
+//         userName: req.body.username,
+//         password: req.body.password,
+//         mail: req.body.mail,
+//         age: req.body.age
+//     }).save();
+// });
+
+router.post('/', function(req, res, next) {
+    User.register( new User({
+        username: req.body.username,
+        mail: req.body.mail,
+        age: req.body.age
+    }), req.body.password, function (err, user) {
+        console.log(err);
+        res.json(user);
+    });
+});
+
+module.exports = router;
---
+++ verify.js
@@ -0,0 +1,27 @@
+var User   = require('./model/user');
+var jwt    = require('jsonwebtoken');
+var config = require('./config');
+
+exports.getToken = function (user) {
+  return jwt.sign(user, config.secretKey);
+};
+
+exports.verifyUser = function (req, res, next) {
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    if(token){
+        jwt.verify(token, config.secretKey, function (err, decoded) {
+            if(err){
+                res.json(err);
+            }
+            req.decoded = decoded;
+            next();
+        });
+    }
+    else{
+        res.json({
+            status: 401,
+            message: "Unauthorized"
+        })
+    }
+};
